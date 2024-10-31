c602cb6d774b97f853e6b14d4f156ce2a65f826e
AgungDH <agungdh@live.com>
2024-10-31 22:22:42 +0700

    FIX: user icon

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -268,7 +268,7 @@
           </div>
           <button
             type="button"
-            class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            class="flex mx-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
             id="user-menu-button"
             aria-expanded="false"
             data-dropdown-toggle="dropdown"
@@ -276,7 +276,7 @@
             <span class="sr-only">Open user menu</span>
             <img
               class="w-8 h-8 rounded-full"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png"
+              src="/user.png"
               alt="user photo"
             />
           </button>

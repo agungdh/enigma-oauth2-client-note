2b4ae6723ba0bc15ca25d4d85c9dd53760b7a1f9
AgungDH <agungdh@live.com>
2024-11-01 14:07:55 +0700

    refresh token

--- app/Http/Middleware/LoggedIn.php
+++ app/Http/Middleware/LoggedIn.php
@@ -16,15 +16,49 @@ class LoggedIn
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!session('access_token')) {
+            return redirect('/');
+        }
+
+        $response = $this->getUser($request);
+        
+        if ($response->failed()) {
+            if($response->unauthorized()) {
+                $responseRefresh = Http::asForm()->post(config('app.oauth_url') . '/oauth/token', [
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => session('refresh_token'),
+                    'client_id' => config('app.oauth_client_id'),
+                    'client_secret' => config('app.oauth_client_secret'),
+                    'scope' => '',
+                ]);
+
+                if ($responseRefresh->successful()) {
+                    $request->session()->put($responseRefresh->json());
+
+                    $response = $this->getUser($request);
+                } else {
+                    $request->session()->invalidate();
+
+                    return redirect('/');
+                }
+            } else {
+                $response->throw();
+            }
+        }
+
+        $request->session()->put('user', $response->json());
+
+        return $next($request);
+    }
+
+    private function getUser(Request $request) {
         $accessToken = $request->session()->get('access_token');
 
         $response = Http::withHeaders([
             'Accept' => 'application/json',
             'Authorization' => 'Bearer '.$accessToken,
         ])->get(config('app.oauth_url') . '/api/user');
-        
-        $request->session()->put('user', $response->json());
 
-        return $next($request);
+        return $response;
     }
 }

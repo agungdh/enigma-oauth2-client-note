96d9fe252590af98fda99f5665221f317ad59fe5
AgungDH <agungdh@live.com>
2024-11-01 15:55:29 +0700

    tukang ketik logo

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -48,9 +48,8 @@
           </button>
           <a href="/" class="flex items-center justify-between mr-4">
             <img
-              src="https://flowbite.s3.amazonaws.com/logo.svg"
+              src="/tukang ketik ava.jpg"
               class="mr-3 h-8"
-              alt="Flowbite Logo"
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Note</span>
           </a>
